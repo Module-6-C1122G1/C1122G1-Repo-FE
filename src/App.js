7cdf3c39dbd7ec253dee65863389b197875775c2
NgocTruongg <ngongoctruong1111ts@gmail.com>
2023-05-27 10:51:54 +0700

    Upload layouts to create and update Customer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home";
-import CommonLayout from "./layout/CommonLayout";
-import Login from "./pages/Login";
-import BookingTicket from "./pages/Booking/BookingTicket";
-import {Demo} from "./components/booking-ticket/Demo";
+import React from "react";
+import {CreateCustomerAccount} from "./components/customer/Create";
+import {UpdateCustomer} from "./components/customer/Update";
 
 function App() {
     return (
-        <Routes>
-            <Route path='' element={<CommonLayout/>}>
-                <Route path='' element={<Home/>}/>
-                <Route path='/booking-ticket' element={<BookingTicket/>}/>
-            </Route>
-            <Route path="/login" element={<Login/>}/>
-        </Routes>
+        <div>
+            <CreateCustomerAccount/>
+            <UpdateCustomer/>
+        </div>
     )
 };
 
