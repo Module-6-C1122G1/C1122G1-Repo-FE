4e4f257735c93f9392d836333474e7d7cc442678
unknown <leducnghia14102003@gmail.com>
2023-05-31 16:24:13 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import CommonLayout from "./layout/CommonLayout";
-import Login from "./pages/Login/Login";
-import ConfirmEmail from "./pages/Login/ConfirmEmail";
-import ResetPassword from "./pages/Login/ResetPassword";
 
-import {ConfirmTicket} from "./components/confirm-ticket/ConfirmTicket";
-import Home from "./pages/Home";
 import React from "react";
-import {CreateFilm} from "./components/film/CreateFilm";
-import List from "./components/film/List";
-
+import Discount from "./components/discount/List"
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<CommonLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin/film/create" element={<CreateFilm/>}></Route>
-        <Route path="/login" element={<Login />} />
-        <Route path="/confirm-email" element={<ConfirmEmail />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path='/confirm-ticket' element={<ConfirmTicket/>}/>
-      </Route>
-    </Routes>
+                <Route path='/discount'element={<Discount/>}></Route>
+            
+        </Routes>
 
   );
 }
--- src/service/discount/DiscountService.js
+++ src/service/discount/DiscountService.js
@@ -1,37 +1,11 @@
 import axios from "axios"
 
-export const findByName = async(value,currentPage) => {
+export const findAll = async () => {
     try {
-        return (await axios.get(`http://localhost:8080/discount/list?name=${value}&_page=${currentPage}`)).data
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-export const findAllDiscount = async () => {
-    try {
-        const result = await axios.get(`http://localhost:8080/discount/list`);
+        const result = await axios.get("http://localhost:8080/discount")
         return result.data
-    } catch (e) {
-        console.log(e)
+    } catch (error) {
+        console.log(error)
     }
-}
 
-export const save = async (discount) => {
-    try {
-        await axios.post(`http://localhost:8080/discount/`, {...discount});
-    } catch (e) {
-        console.log(e)
-    }
-}
-
-
-export const remove = async (id) => {
-    try {
-        await axios.delete(`http://localhost:8080/discount/${id}`)
-        alert("Xóa khách hàng thành công!!")
-    } catch (e) {
-        console.log(e)
-    }
 }
