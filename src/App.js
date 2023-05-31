07b987ea72c543f04ecefabf66a0d0f3da9be63d
TrongNghia152004 <ttn.hdttb152004@gmail.com>
2023-05-31 13:35:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-// import logo from './logo.svg';
 import './App.css';
-// import { Routes, Route } from 'react-router-dom';
-// import Home from "./pages/Home";
-// import CommonLayout from "./layout/CommonLayout";
-// import Login from "./pages/Login";
-// import BookingTicket from "./pages/Booking/BookingTicket";
-// import {Demo} from "./components/booking-ticket/Demo";
+import {UpdateEmployee} from "./components/employee/Update";
+import {Route, Routes} from "react-router-dom";
 import {CreateEmployee} from "./components/employee/Create";
 
 function App() {
     return (
-        // <Routes>
-        //     <Route path='' element={<CommonLayout/>}>
-        //         <Route path='' element={<Home/>}/>
-        //         <Route path='/booking-ticket' element={<BookingTicket/>}/>
-        //     </Route>
-        //     <Route path="/login" element={<Login/>}/>
-        // </Routes>
-        <>
-        <CreateEmployee/>
-        </>
+        <Routes>
+            <Route path='/create' element={<CreateEmployee/>}/>
+            <Route path='/update/:id' element={<UpdateEmployee/>}/>
+        </Routes>
     )
-};
+}
 
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getStorage} from "@firebase/storage";
+import 'firebase/storage';
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +21,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getAnalytics(app);
+export const storage = getStorage(app);
