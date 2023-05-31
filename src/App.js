16c240735657157050c1ca73bf652bd889d9205f
unknown <leducnghia14102003@gmail.com>
2023-05-31 16:38:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 import React from "react";
-import Discount from "./components/discount/List"
+import DiscountList from "./components/discount/ListDiscount"
 
 function App() {
   return (
     <Routes>
-                <Route path='/discount'element={<Discount/>}></Route>
+                <Route path='/discount'element={<DiscountList/>}></Route>
             
         </Routes>
 
