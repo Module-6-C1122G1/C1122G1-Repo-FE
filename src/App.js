eb22b1a67b581711774a976ce1d9f0fecc88af54
minhnguyen0910 <minhn091099@gmail.com>
2023-06-01 08:37:32 +0700

    MinhNV up code

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Login from "./pages/Login/Login";
 import ConfirmEmail from "./pages/Login/ConfirmEmail";
 import ResetPassword from "./pages/Login/ResetPassword";
 import BookingTicket from "./pages/Booking/BookingTicket";
+import {DetailDiscount} from "./components/discount/DetailDiscount";
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
                 <Route path='/booking-ticket' element={<BookingTicket/>}/>
                 <Route path='/discount/create' element={<CreateDiscount/>}/>
                 <Route path='/discount/update/:id' element={<UpdateDiscount/>}/>
+                <Route path='/detail-discount/:id' element={<DetailDiscount/>}/>
             </Route>
         </Routes>
 
