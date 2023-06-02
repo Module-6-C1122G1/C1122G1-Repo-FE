ec35172db83e59ff208b0e30f312ebd52ae363a3
HoangThiNhuQuynh <hoangnhuquynh.116@gmail.com>
2023-06-02 14:07:47 +0700

    up

--- dn-cinema-fe-react/src/components/information_ticket/TicketPrint.js
+++ dn-cinema-fe-react/src/components/information_ticket/TicketPrint.js
@@ -15,38 +15,39 @@ export function TicketPrint(props) {
                             <p>Địa chỉ: 280 Trần Hưng Đạo, An Hải Tây, Sơn Trà, Đà Nẵng</p>
                         </div>
                         <div className="ticket-print"></div>
+                        {/*<hr className="text-movie xxx" />*/}
                         <h3 className="text-center text-movie">DN Cinema</h3>
                         <div className="detail-rating p-3">
                             <div>
-                                <h5 className="text-center">{props.ticketDetail.seat.showTime.film.nameFilm}</h5>
+                                <h5 className="text-center">{props.ticketDetail.ticketSet[0].seat.showTime.film.nameFilm}</h5>
                             </div>
                             <table>
                                 <thead>
                                 {/*<tr style={{height: 39}}>*/}
-                                    {/*<th className="text-secondary">Tên phim:</th>*/}
-                                    {/*<h2>{props.ticketDetail.seat.showTime.film.nameFilm}</h2>*/}
+                                {/*<th className="text-secondary">Tên phim:</th>*/}
+                                {/*<h2>{props.ticketDetail.seat.showTime.film.nameFilm}</h2>*/}
                                 {/*</tr>*/}
                                 </thead>
                                 <tbody>
                                 <tr style={{height: 39}}>
                                     <th className="text-secondary">Rạp:</th>
-                                    <td>{props.ticketDetail.seat.showRoom.nameShowRoom}</td>
+                                    <td>{props.ticketDetail.ticketSet[0].seat.showRoom.nameShowRoom}</td>
                                 </tr>
                                 <tr style={{height: 39}}>
                                     <th className="text-secondary">Ngày chiếu :</th>
-                                    <td style={{fontFamily: "Roboto"}}>{props.ticketDetail.seat.showTime.film.dateStartFilm}</td>
+                                    <td style={{fontFamily: "Roboto"}}>{props.ticketDetail.ticketSet[0].seat.showTime.film.dateStartFilm}</td>
                                 </tr>
                                 <tr style={{height: 39}}>
                                     <th className="text-secondary">Giờ chiếu:</th>
-                                    <td>{props.ticketDetail.seat.showTime.showTime}</td>
+                                    <td>{props.ticketDetail.ticketSet[0].seat.showTime.showTime}</td>
                                 </tr>
                                 <tr style={{height: 39}}>
                                     <th className="text-secondary">Ghế:</th>
-                                    <td>{props.ticketDetail.seat.nameSeat}</td>
+                                    <td>{props.ticketDetail.ticketSet[0].seat.nameSeat}</td>
                                 </tr>
                                 <tr className="" style={{height: 39}}>
                                     <th className="text-secondary">Giá:</th>
-                                    <td>{props.ticketDetail.priceAfterDiscount}</td>
+                                    <td>{props.ticketDetail.ticketSet[0].priceAfterDiscount}</td>
                                 </tr>
                                 </tbody>
                             </table>
